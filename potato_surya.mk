9b89a26b472339b085a57b1b08bdc7704097b625
Astile97 <zilliondollarkuruvi@gmail.com>
2021-01-13 18:31:20 +0530

    surya: Potatoes * Systemwide Signed-off-by: Astile97 <zilliondollarkuruvi@gmail.com>

--- potato_surya.mk
+++ potato_surya.mk
@@ -6,11 +6,16 @@
 
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit from Potato vendor.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+
+# GApps configurations
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_surya
+PRODUCT_NAME := potato_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
@@ -18,4 +23,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-HAVOC_BUILD_TYPE := Official
+
